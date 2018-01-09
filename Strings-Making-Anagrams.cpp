1443210c7323869d7ebe9642174b30f615a2d224
ivanug <sempijjawork@users.noreply.github.com>
2018-01-09 14:42:59 +0300

    Create Strings-Making-Anagrams.cpp

---
+++ Strings-Making-Anagrams.cpp
@@ -0,0 +1,8 @@
+int number_needed(string a, string b) {
+    auto count = 0;
+    vector<int> freq(26, 0);
+    for (auto c : a) { ++freq[c - 'a']; }
+    for (auto c : b) { --freq[c - 'a']; }
+    for (auto val : freq) { count += abs(val); }
+    return count;
+}
